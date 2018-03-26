6c5060adc338a0e81a7c30d762b6d536eda46926
yz1339 <yz1339@nyu.edu>
2018-03-26 14:49:57 -0500

    draw, clear added

--- sensorCode/sensorCode.ino
+++ sensorCode/sensorCode.ino
@@ -1,26 +1,40 @@
 #define SENSORPINA A0 // x axis
- //TODO: define other sensor inputs
+#define SENSORPINB A1 // y axis
+#define SENSORPINRST A2 // rst axis
 
 unsigned long targetTime=0;
-const unsigned long interval=2500; //TODO: How fast should we read
+const unsigned long interval=100; //TODO: How fast should we read
+int x;
+int y;
+int rstValue;
 void setup(){
-// TODO: begin the serial connection with a baudrate of 115200
+  pinMode(SENSORPINA, INPUT);
+  pinMode(SENSORPINB, INPUT);
+  pinMode(SENSORPINRST, INPUT);
+  Serial.begin(115200);
+  
 }
 
 
 void loop(){
-	if(millis()>=targetTime){
-		targetTime= millis()+interval;
-		Serial.println(analogRead(SENSORPINA));
+  if(millis()>=targetTime){
+    targetTime= millis()+interval;
+    //Add other sensor read outs
+    x = analogRead(SENSORPINA);
+    y = analogRead(SENSORPINB);
+    rstValue = analogRead(SENSORPINRST);
 
-		 //TODO: Add other sensor read outs
-     //TODO: convert values into a string https://www.arduino.cc/en/Tutorial/StringConstructors
-		 //TODO: combine them into a string that can be understood by server.js
-		 //TODO: send the string over serial
+    // convert values into a string, combine them into a string that can be understood by server.js
+    // send the string over serial
+    String newPosition = String(x, DEC) + "," + String(y, DEC);      
+    Serial.println(newPosition);
 
+    if (rstValue > 10) {
+      Serial.println("rst");
+    }
+  }
+  // TODO: Detect if you want to reset the screen(shake the etch-a-sketch)
 
-	}
-	// TODO: Detect if you want to reset the screen(shake the etch-a-sketch)
   // TODO: write the reset message(see server.js) to the serial port
 
 }
