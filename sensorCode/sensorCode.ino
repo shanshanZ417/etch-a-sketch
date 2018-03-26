c26569a8e43740d3297f26f00fa7d93bb62ba2bc
yz1339 <yz1339@nyu.edu>
2018-03-26 16:59:35 -0500

    from arduino to server when input changes

--- sensorCode/sensorCode.ino
+++ sensorCode/sensorCode.ino
@@ -4,15 +4,16 @@
 
 unsigned long targetTime=0;
 const unsigned long interval=100; //TODO: How fast should we read
-int x;
-int y;
+int x = 100;
+int y = 100;
 int rstValue;
+int prev_x = 100;
+int prev_y = 100;
 void setup(){
   pinMode(SENSORPINA, INPUT);
   pinMode(SENSORPINB, INPUT);
   pinMode(SENSORPINRST, INPUT);
   Serial.begin(115200);
-  
 }
 
 
@@ -23,12 +24,14 @@ void loop(){
     x = analogRead(SENSORPINA);
     y = analogRead(SENSORPINB);
     rstValue = analogRead(SENSORPINRST);
-
-    // convert values into a string, combine them into a string that can be understood by server.js
-    // send the string over serial
-    String newPosition = String(x, DEC) + "," + String(y, DEC);      
-    Serial.println(newPosition);
-
+    if (x != prev_x || y != prev_y) {
+      // convert values into a string, combine them into a string that can be understood by server.js
+      // send the string over serial
+      String newPosition = String(x, DEC) + "," + String(y, DEC);      
+      Serial.println(newPosition);
+      prev_x = x;
+      prev_y = y;
+    }
     if (rstValue > 10) {
       Serial.println("rst");
     }
